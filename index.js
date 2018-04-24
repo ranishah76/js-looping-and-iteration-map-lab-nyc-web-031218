87fe06b84f5ccfaff3724f03ae47ac1191b95423
Rani Shah <rani.shah@flatironschool.com>
2018-04-24 00:13:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+ return drivers.map( function(driver) {
+   return driver.toLowerCase()
+ })
+}
+
+function nameToAttributes(list) {
+  return list.map(function(element) {
+    const firstname =  element.split(' ')[0];
+    const lastname =  element.split(' ')[1];
+    return { firstName: firstname, lastName: lastname};
+  })
+}
+
+function attributesToPhrase(list) {
+  return list.map(function (person) {
+    return `${person.name} is from ${person.hometown}`
+  })
+}
